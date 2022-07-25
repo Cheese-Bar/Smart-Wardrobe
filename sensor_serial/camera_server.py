be9fd1226f4367fb03cfc95ec5e9bf1ec1e45722
Cheese-Bar <11911618@mail.sustech.edu.cn>
2022-07-25 23:05:47 +0800

    test camera

--- sensor_serial/camera_server.py
+++ sensor_serial/camera_server.py
@@ -10,7 +10,7 @@ frame = 1
 while True:
     try:
         button.wait_for_press()
-        camera.capture('/home/p07/temp_fig/frame%03d.jpg' % frame)
+        camera.capture('/temp_imgs/frame%03d.jpg' % frame)
         frame += 1
     except KeyboardInterrupt:
         camera.stop_preview()
