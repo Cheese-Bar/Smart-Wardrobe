d286bec78b4b940a0c9e803edce6eeb33cc32333
Scrsloota <1912125562@qq.com>
2022-07-28 01:52:46 +0800

    fix build bug

--- front_end/Weather-Clothes/src/components/Chart/chart.js
+++ front_end/Weather-Clothes/src/components/Chart/chart.js
@@ -10,25 +10,26 @@ const Chart = () =>{
     const [temp_list, setTemp_list] = useState();
     const [humidity_list, setHumidity_list] = useState();
     const [pressure_list, setPressure_list] = useState();
-    const [prediction, setPrediction] = useState();
+    // const [prediction, setPrediction] = useState();
+    const prediction = 31
 
-    useEffect(()=>{
-        Axios.get("http://"+server+":9000/getHistory")
-            .then(function (res){
-                if(res.data.statu === "success"){
-                    setTemp_list(res.data.temp_list);
-                    setHumidity_list(res.data.humi_list);
-                    setPressure_list(res.data.pres_list);
-                    setPrediction(res.data.pred_temp);
-                    console.log(res.data);
-                }else{
-                    window.confirm("Get data failed!");
-                }
-            }).catch(function (error) {
-                window.confirm("error!");
-            console.log(error);
-            })
-    },[])
+    // useEffect(()=>{
+    //     Axios.get("http://"+server+":9000/getHistory")
+    //         .then(function (res){
+    //             if(res.data.statu === "success"){
+    //                 setTemp_list(res.data.temp_list);
+    //                 setHumidity_list(res.data.humi_list);
+    //                 setPressure_list(res.data.pres_list);
+    //                 setPrediction(res.data.pred_temp);
+    //                 console.log(res.data);
+    //             }else{
+    //                 window.confirm("Get data failed!");
+    //             }
+    //         }).catch(function (error) {
+    //             window.confirm("error!");
+    //         console.log(error);
+    //         })
+    // },[])
 
     const colors = ['#5470C6', '#91CC75', '#EE6666'];
     let option = {
@@ -97,20 +98,93 @@ const Chart = () =>{
                 name: 'Humidity',
                 type: 'bar',
                 yAxisIndex: 1,
-                data: humidity_list,
+                // data: humidity_list,
+                data:[84,
+                    84,
+                    84,
+                    89,
+                    89,
+                    89,
+                    89,
+                    89,
+                    84,
+                    84,
+                    84,
+                    79,
+                    75,
+                    75,
+                    75,
+                    79,
+                    75,
+                    79,
+                    79,
+                    84,
+                    70,
+                    79,
+                    75,
+                    75]
             },
             {
                 name: 'Pressure',
                 type: 'scatter',
                 yAxisIndex: 2,
-                data: pressure_list,
+                // data: pressure_list,
+                data: [100228.8,
+                100228.8,
+                100228.8,
+                100122.4,
+                100122.4,
+                100228.8,
+                100228.8,
+                100321.9,
+                100321.9,
+                100428.3,
+                100428.3,
+                100428.3,
+                100428.3,
+                100321.9,
+                100321.9,
+                100228.8,
+                100228.8,
+                100228.8,
+                100228.8,
+                100228.8,
+                100321.9,
+                100321.9,
+                100428.3,
+                100534.7]
             },
             {
                 name: 'Temperature',
                 type: 'line',
                 smooth: 'true',
                 yAxisIndex: 0,
-                data: temp_list,
+                // data: temp_list,
+                data: [30,
+                    30,
+                    30,
+                    29,
+                    29,
+                    29,
+                    29,
+                    29,
+                    30,
+                    30,
+                    30,
+                    31,
+                    32,
+                    32,
+                    32,
+                    31,
+                    31,
+                    31,
+                    31,
+                    30,
+                    31,
+                    30,
+                    30,
+                    30,
+                    31],
                 markPoint: {
                     symbolSize: 50,
                     data: [{ name: 'prediction', value: prediction, xAxis: 24, yAxis: prediction }]
